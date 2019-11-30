a0a69d43696d7d39d8c59dc4a8c58ab84f0f9dbd
DanB-Web <dan.bayford@gmail.com>
2019-11-30 14:18:06 +0000

    Final version for review
    Final version of List FIlter and Pagination for Slack review

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+/*Useful global variables and class assignments*/
+
 const list = document.getElementsByClassName("student-item");
+const searchResults = document.getElementsByClassName("matched");
 const mainPage = document.querySelector(".page"); 
 const links = document.getElementsByTagName("a");
+const header2 = document.querySelector("h2");
 
-/*Function to iterate through array and display only those that meet arguments*/
+header2.className = "title";
+
+
+/*Function to iterate through student list array and display only those that meet the arguments for related page number
+- it calculates the required number of pages from the list.length property, and then uses an if...else loop to
+set the style.display property of each element in the array*/
 
 function showPage (list, page) {
 
@@ -23,16 +32,20 @@ function showPage (list, page) {
           else {
 
             list[i].style.display = "none";}
-
     }
-
 }
 
-/*Function to create and append populated div when called*/
+/*Function to create and append/insert populated page number div when called - it create a div to
+append to the bottom of the main page, and uses a for...loop to populate a list inside the 
+new div with an li for each required page. The li contains the relevant <a>. A final if... 
+statement checks if the div needs to be appended to the main page (on initial page load) or
+if it needs to replace an existing div*/
 
 function appendPageLinks (list) {
 
-  
+  /*Create a div including page number ul, 
+  based on passed list.length parameter*/
+
   const items = list.length;
   const pages = Math.ceil(items * 0.1);
 
@@ -51,7 +64,55 @@ function appendPageLinks (list) {
   }
 
   div.appendChild(ul);
-  mainPage.appendChild(div);
+
+  /*Check if div needs to be appended or inserted*/
+
+  const check = !!document.querySelector(".pagination");
+
+  if (check)
+  {
+    const pageNumbers = document.querySelector(".pagination");
+    mainPage.replaceChild(div, pageNumbers);}
+
+  else {mainPage.appendChild(div)}
+
+}
+
+/*Function to search records - checks through records and sets its style.display
+property and class depending on if it finds the neccessry search term. Matched 
+classes are passed into the 'send' array to append the correct number of page links
+via the appendPageLinks function. If no matches are found, the header display alerts
+the user and the function is exited early. */
+
+function searchName (searchInput, list) {
+
+  let send = []; 
+
+  for (let i = 0; i < list.length; i++) 
+
+  {
+  
+          if (searchInput.length != 0 && list[i].textContent.toLowerCase().includes(searchInput))
+              
+          
+          { list[i].style.display = " ";
+            list[i].classList.add("matched");
+            send.push(list[i]);}
+
+          else {
+          list[i].style.display = "none";}
+
+  }
+
+  if (send.length == 0)
+  {
+    header2.textContent = "No results found!";
+    appendPageLinks(0);
+    return;
+  }
+
+  appendPageLinks(send);
+  links[0].className = "active";
 
 }
 
@@ -61,14 +122,29 @@ showPage(list, 1);
 appendPageLinks(list);
 links[0].className = "active";
 
+/*Dynamically create search bar and append it to title div*/
 
-/*Add event listener to pass relevent parameters to showPage and appendPageLinks functions*/
+const title = document.querySelector(".page-header");
+const searchDiv = document.createElement("div");
+const searchButton = document.createElement("button");
+const searchBox = document.createElement("input");
 
-const pages = document.querySelector(".pagination")
+searchDiv.className = "student-search";
+searchButton.textContent = "Search";
+searchBox.type = "text";
+searchBox.placeholder = "Search student records...";
+
+searchDiv.appendChild(searchBox);
+searchDiv.appendChild(searchButton);
+title.appendChild(searchDiv);
+
+
+/*Add event listener to pass relevent parameters to showPage and appendPageLinks functions when page numbers clicked
+- it also sets the clicked page number className to 'active' for styling*/
+
+const pages = document.querySelector(".pagination");
 
 pages.addEventListener ("click", (event) => {
-
-  //const links = document.getElementsByTagName("a");
 
   for (let i = 0; i < links.length; i++ )
 
@@ -77,7 +153,42 @@ pages.addEventListener ("click", (event) => {
   }
 
   event.target.className = "active";
-
   showPage (list, event.target.textContent)
 
 });
+
+
+/*Event listener for the search button - either sends the search box contnent to the searchName
+function, or resets the page to its original condition, depending on page state*/
+
+searchButton.addEventListener('click', (event) => {
+
+  if (searchButton.textContent === "Search"){
+
+    event.preventDefault();
+    const searchInput = searchBox.value.toLowerCase();
+    searchName(searchInput, list);
+    searchButton.textContent = "Reset"}
+
+  else {
+ 
+    // showPage(list, 1);
+    // appendPageLinks(list);
+    // header2.textContent = "Students";
+    // links[0].className = "active";
+    // searchButton.textContent = "Search";
+
+    // for (i = 0; i < list.length; i++)
+    // {list[i].classList.remove("matched");}
+    // console.log(searchResults);
+
+    location.reload();
+
+  }
+
+});
+
+
+
+
+        
